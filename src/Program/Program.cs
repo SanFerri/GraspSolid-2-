8664b9ff14f0d2ec19cf484d2199efadc73bfb84
SanFerri <santiago.ferraro@correo.ucu.edu.uy>
2022-04-21 10:31:49 -0300

    ultimo comentario

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -25,7 +25,8 @@ namespace Full_GRASP_And_SOLID
             recipe.FinalProduct = GetProduct("Café con leche");
             recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
             recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
-            // Aqui
+            // Aquí en vez de usar recipe.Print(), ConsolePrinter es quien se encarga de imprimir
+            // la receta.
             ConsolePrinter.Print(recipe.GetRecipeText());
         }
 
